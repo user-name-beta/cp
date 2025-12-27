f86ea36c191403d0e4b149de267e469801ffd56e
user-name-beta <john202312@126.com>
2025-12-27 16:18:15 +0800

    Add src/main.h and src/main.c(not a placeholder longer)
    Print version when no command is given in main.c
    
    Add more error info for users in main.c

---
+++ src/main.h
@@ -0,0 +1,27 @@
+// main.h
+//
+// Copyright (c) 2025 user-name-beta. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+//
+// Provide with the entry point function CP_Main() for the program.
+
+#ifndef _CP_MAIN_H_
+#define _CP_MAIN_H_
+
+#ifdef _CP_MAIN_E_
+#define CP_EXPORT_SYMBOL
+#endif
+#include "exports.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+CP_API_FUNC(int) CP_Main(int argc, char **argv);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _CP_MAIN_H_ */
