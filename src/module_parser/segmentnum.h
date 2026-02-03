c96d8bd188dfe177c0fb22904bd22c34349cbf38
user-name-beta <john202312@126.com>
2026-02-03 12:19:44 +0800

    Add segmentnum.h
    for getting seg index in the header seg using macros

---
+++ src/module_parser/segmentnum.h
@@ -0,0 +1,18 @@
+// segmentnum.h
+//
+// Copyright (c) 2025 user-name-beta. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+//
+// Define the segmentation index.
+
+#ifndef _CP_MODULE_PARSER__SEGMENTNUM_H_
+#define _CP_MODULE_PARSER__SEGMENTNUM_H_
+
+#define CPMODULE_EXPORT_TABLE_INDEX 0
+#define CPMODULE_IMPORT_TABLE_INDEX 1
+#define CPMODULE_STRING_TABLE_INDEX 2
+#define CPMODULE_DATA_SEGMENT_INDEX 3
+#define CPMODULE_CODE_SEGMENT_INDEX 4
+
+#endif /* _CP_MODULE_PARSER__SEGMENTNUM_H_ */
