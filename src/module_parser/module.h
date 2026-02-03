d93f969acbff9c64095f5a1ceadfeb24cc2425d9
user-name-beta <john202312@126.com>
2026-02-03 18:09:05 +0800

    Add module.h

---
+++ src/module_parser/module.h
@@ -0,0 +1,19 @@
+// module_parser.h
+//
+// Copyright (c) 2025 user-name-beta. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+//
+// A header file including other headers in this directory.
+
+#ifndef _CP_MODULE_PARSER__MODULE_H_
+#define _CP_MODULE_PARSER__MODULE_H_
+
+#include <stdint.h>
+
+typedef struct
+{
+    uint8_t byte_mode;
+} CPModule;
+
+#endif /* _CP_MODULE_PARSER__MODULE_H_ */
