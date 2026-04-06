7f2b7b111afcc274479424c8f96e0f507c7a5061
Huang Jiangyao <john202312@126.com>
2026-04-06 12:05:24 +0800

    Change copyright notice (#116)
    Change the MIT License notice to GPL License notice.

--- src/cpc/main.c
+++ src/cpc/main.c
@@ -1,10 +1,20 @@
-// main.c
-//
-// Copyright (c) 2025-2026 user-name-beta. All rights reserved.
-// Licensed under the MIT license.
-// See LICENSE file in the project root for full license information.
-//
-// Implementation of the entry point function CP_Main() for the program.
+/*
+ * main.c - entry point of the cpc program.
+ * Copyright (C) 2025-2026 Huang Jiangyao. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #define _CP_CPC__MAIN_E_
 
 #include <parsearg.h>
